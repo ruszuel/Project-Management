c730c526b88bee2e89a33e2acd51e15fc0e1b806
Russuel <johnlloydrussuel.tengco@gmail.com>
2024-11-26 23:30:03 +0800

    initial

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import {Routes, Route} from 'react-router-dom'
+import { Home } from './components/Home'
+import Login from './components/Login'
+function App() {
+  return (
+    <div className='App'>
+      <Routes>
+        <Route path='' element={<Home/>}/>
+        <Route path='/login' element={<Login/>}/>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
